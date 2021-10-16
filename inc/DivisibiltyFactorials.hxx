68e8d0ccea0c6358ab0b6027da34472bab3f79fb
Mike <mike@pop-os.localdomain>
2021-10-16 18:43:38 +0200

    Restore point for dev folder.

---
+++ inc/DivisibiltyFactorials.hxx
@@ -0,0 +1,15 @@
+/*
+ * DivisibiltyFactorials.h++
+ * 
+ * Copyright 2020 mike <mike@pop-os>
+ * 
+ * ~/.config/geany/templates/files/main.hxx
+ */
+
+#ifndef __DivisibiltyFactorials_hxx__
+#define __DivisibiltyFactorials_hxx__
+
+// Declarations, typedefs, enum, defines
+
+#endif
+
---
+++ inc/library.hxx
@@ -0,0 +1,14 @@
+
+/*
+ * library.h++
+ * ~/.config/geany/templates/files/main.hxx
+ */
+
+#ifndef __library_hxx__
+#define __library_hxx__
+
+// Declarations, typedefs, enum, defines
+void lib_func(void);
+
+#endif
+
