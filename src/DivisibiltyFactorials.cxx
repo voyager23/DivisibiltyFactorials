093fdda5884b31b2a90bd055e3044edf136e8c1b
Mike <mike@pop-os.localdomain>
2021-10-16 09:41:31 +0200

    setup

---
+++ src/DivisibiltyFactorials.cxx
@@ -0,0 +1,18 @@
+/*
+ * DivisibiltyFactorials.c++
+ * 
+ * Copyright 2020 mike <mike@pop-os>
+ * 
+ * ~/.config/geany/templates/files/main.c++
+ */
+
+#include <iostream>
+
+#include "../inc/DivisibiltyFactorials.hxx"
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
